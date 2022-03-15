b17e87bad42e934df56dfa47442e78e4f51c589e
sukhvinderkaur26 <sukhvinder2630@gmail.com>
2022-03-15 08:34:51 -0600

    New project added

---
+++ src/com/company/Father.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Father {
+    private String familySurname;
+    private String address;
+    private int assetValue;
+
+    public String getFamilySurname() {
+        return familySurname;
+    }
+
+    public void setFamilySurname(String familySurname) {
+        this.familySurname = familySurname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAssetValue() {
+        return assetValue;
+    }
+
+    public void setAssetValue(int assetValue) {
+        this.assetValue = assetValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Father{" +
+                "familySurname='" + familySurname + '\'' +
+                ", address='" + address + '\'' +
+                ", assetValue=" + assetValue +
+                '}';
+    }
+
+    public Father(String familySurname, String address, int assetValue) {
+        this.familySurname = familySurname;
+        this.address = address;
+        this.assetValue = assetValue;
+
+    }
+}
